f0d73795108cf61515f7e14a1e5e913a861c2638
chenyinjie0810 <chenyinjie0810@qq.com>
2019-10-27 03:44:17 +0800

    jpa基础查询

---
+++ chenyj_article/src/main/java/com/chenyj/article/controller/ColumnController.java
@@ -0,0 +1,101 @@
+package com.chenyj.article.controller;
+
+import com.chenyj.article.pojo.Column;
+import com.chenyj.article.service.ColumnService;
+import entity.PageResult;
+import entity.Result;
+import enums.StatusCodeEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+/**
+ * 控制器层
+ * @author Administrator
+ *
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/column")
+public class ColumnController {
+
+	@Autowired
+	private ColumnService columnService;
+	
+	
+	/**
+	 * 查询全部数据
+	 * @return
+	 */
+	@RequestMapping(method= RequestMethod.GET)
+	public Result findAll(){
+		return new Result(StatusCodeEnum.SUCCESS,columnService.findAll());
+	}
+	
+	/**
+	 * 根据ID查询
+	 * @param id ID
+	 * @return
+	 */
+	@RequestMapping(value="/{id}",method= RequestMethod.GET)
+	public Result findById(@PathVariable String id){
+		return new Result(StatusCodeEnum.SUCCESS,columnService.findById(id));
+	}
+
+
+	/**
+	 * 分页+多条件查询
+	 * @param searchMap 查询条件封装
+	 * @param page 页码
+	 * @param size 页大小
+	 * @return 分页结果
+	 */
+	@RequestMapping(value="/search/{page}/{size}",method=RequestMethod.POST)
+	public Result findSearch(@RequestBody Map searchMap , @PathVariable int page, @PathVariable int size){
+		Page<Column> pageList = columnService.findSearch(searchMap, page, size);
+		return  new Result(StatusCodeEnum.SUCCESS,  new PageResult<Column>(pageList.getTotalElements(), pageList.getContent()) );
+	}
+
+	/**
+     * 根据条件查询
+     * @param searchMap
+     * @return
+     */
+    @RequestMapping(value="/search",method = RequestMethod.POST)
+    public Result findSearch( @RequestBody Map searchMap){
+        return new Result(StatusCodeEnum.SUCCESS,columnService.findSearch(searchMap));
+    }
+	
+	/**
+	 * 增加
+	 * @param column
+	 */
+	@RequestMapping(method=RequestMethod.POST)
+	public Result add(@RequestBody Column column  ){
+		columnService.add(column);
+		return new Result(StatusCodeEnum.SUCCESS,"增加成功");
+	}
+	
+	/**
+	 * 修改
+	 * @param column
+	 */
+	@RequestMapping(value="/{id}",method= RequestMethod.PUT)
+	public Result update(@RequestBody Column column, @PathVariable String id ){
+		column.setId(id);
+		columnService.update(column);		
+		return new Result(StatusCodeEnum.SUCCESS,"修改成功");
+	}
+	
+	/**
+	 * 删除
+	 * @param id
+	 */
+	@RequestMapping(value="/{id}",method= RequestMethod.DELETE)
+	public Result delete(@PathVariable String id ){
+		columnService.deleteById(id);
+		return new Result(StatusCodeEnum.SUCCESS,"删除成功");
+	}
+	
+}
---
+++ chenyj_article/src/main/java/com/chenyj/article/dao/ArticleDao.java
@@ -0,0 +1,14 @@
+package com.chenyj.article.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.chenyj.article.pojo.Article;
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface ArticleDao extends JpaRepository<Article,String>,JpaSpecificationExecutor<Article>{
+	
+}
